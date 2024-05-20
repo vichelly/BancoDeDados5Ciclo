887fdf57be5010e1450c24573e8eddd82e1f5cf1
Vitor <vlucasff@hotmail.com>
2024-05-20 17:09:48 -0300

    s

--- item4.sql
+++ item4.sql
@@ -2,4 +2,4 @@ SELECT
   p.nome AS nome_professor,
   d.nome AS nome_departamento
 FROM Professores p
-INNER JOIN Departamentos d ON p.id_professor = d.id_chefe_professor;  -- Corrected join column
+INNER JOIN Departamentos d ON p.id_professor = d.id_chefe_professor;
